fe3d33c2a5b12c4c288e48d566ace53c383c9823
Mario Villaplana <mario.villaplana@gmail.com>
2018-07-06 18:10:48 -0500

    Add notes up to recursive data structures and hindent format

--- funcy.hs
+++ funcy.hs
@@ -1,38 +1,57 @@
 import Data.Char
+import qualified Data.Map as Map
 
 doubleMe x = x + x
+
 doubleUs x y = doubleMe x + doubleMe y
 
 -- if always needs else, because if is a function that must "return" something
 doubleSmallNumber x =
-    if x > 100
-        then x
-        else x * 2 -- list comprehension with a predicate
-boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+  if x > 100
+    then x
+    else x * 2 -- list comprehension with a predicate
+
+boomBangs xs =
+  [ if x < 10
+    then "BOOM!"
+    else "BANG!"
+  | x <- xs
+  , odd x
+  ]
 
 -- tuples can have more than 1 type, lists can't
-
 -- Example combining tuples and list comprehensions
 -- Find right triangle with integers for all sides, sides <= 10, perimeter 24
-
 -- All triangles with integers <= 10 for sides
-triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
+triangles = [(a, b, c) | c <- [1 .. 10], b <- [1 .. 10], a <- [1 .. 10]]
+
 -- Right triangles of the above
-rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b],
-    a^2 + b^2 == c^2]
+rightTriangles =
+  [ (a, b, c)
+  | c <- [1 .. 10]
+  , b <- [1 .. c]
+  , a <- [1 .. b]
+  , a ^ 2 + b ^ 2 == c ^ 2
+  ]
+
 -- Perimeter == 24 of the above
-rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b],
-    a^2 + b^2 == c^2, a+b+c==24]
+rightTriangles' =
+  [ (a, b, c)
+  | c <- [1 .. 10]
+  , b <- [1 .. c]
+  , a <- [1 .. b]
+  , a ^ 2 + b ^ 2 == c ^ 2
+  , a + b + c == 24
+  ]
 
 -- type declarations before functions
 removeNonUppercase :: [Char] -> [Char]
-removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+removeNonUppercase st = [c | c <- st, c `elem` ['A' .. 'Z']]
+
 -- it wouldn't compile if i put this above it instead:
 -- removeNonUppercase :: [Char] -> Char
-
 -- if unsure, can open up intepreter and do :t function to find out type of
 -- function. can actually do that w/ any expression.
-
 -- let's test out a type declaration quick
 circumference :: Double -> Double
 circumference r = 2 * pi * r -- works with 4 and 4.0
@@ -43,11 +62,9 @@ reverseCons :: [a] -> a -> [a]
 reverseCons list a = list ++ [a]
 
 -- typeclasses
-
 -- example: "elem" function for testing if something is a member of list, :t:
 -- (Eq a) => a -> [a] -> Bool
 -- a can be anything that can be equal to another thing
-
 -- sometimes, for something ambiguous, a type annotation is needed -
 -- "4" is a string, and we can convert it into something readable (something in
 -- typeclass Read)
@@ -57,7 +74,6 @@ reverseCons list a = list ++ [a]
 -- read "4" :: Int
 -- or
 -- read "4" :: Float
-
 -- pattern matching, evaluated top to bottom
 lucky :: (Integral a) => a -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
@@ -70,9 +86,7 @@ factorial n = n * factorial (n - 1)
 
 -- always include a catch-all pattern, or your program will crash with
 -- unexpected input
-
 -- pattern matching with vector addition
-
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
@@ -90,80 +104,73 @@ capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 -- like many chained if-else statements
 bmiTell :: (RealFloat a) => a -> a -> String
 bmiTell weight height
-    | bmi <= 18.5 = "underweight"
-    | bmi <= 25.0 = "healthy"
-    | bmi <= 30.0 = "overweight"
-    | otherwise   = "obese"
-    where bmi = weight / height ^ 2
+  | bmi <= 18.5 = "underweight"
+  | bmi <= 25.0 = "healthy"
+  | bmi <= 30.0 = "overweight"
+  | otherwise = "obese"
+  where
+    bmi = weight / height ^ 2
+
 -- define vars at end of function with "where"
-
 -- make your own max function
-
 max' :: (Ord a) => a -> a -> a
 max' a b
-    | a > b     = a
-    | otherwise = b
+  | a > b = a
+  | otherwise = b
 
 -- can also do function definitions inline
 -- Make your own compare function
 marioCompare :: (Ord a) => a -> a -> Ordering
 a `marioCompare` b
-    | a > b     = GT
-    | a == b    = EQ
-    | otherwise = LT
+  | a > b = GT
+  | a == b = EQ
+  | otherwise = LT
 
 -- initials
 initials :: String -> String -> String
 initials firstname lastname = [toUpper f] ++ ". " ++ [toUpper l] ++ "."
-    where (f:_) = firstname
-          (l:_) = lastname
--- can pattern match with "where", too
+  where
+    (f:_) = firstname
+    (l:_) = lastname
 
+-- can pattern match with "where", too
 {-
 calcBmis :: (RealFloat a) => [(a, a)] -> [a]
 calcBmis xs = [bmi w h | (w, h) <- xs]
     where bmi weight height = weight / height ^ 2
 --}
-
 -- "where" bindings bind variables at end of function. If you need something
 -- more localized, i.e. something that won't span across guards, use "let"
 -- bindings. "where" bindings span across the whole function, including across
 -- guards.
-
 -- see this used to calc a cylinder's surface area
 cylinder :: (RealFloat a) => a -> a -> a
 cylinder r h =
-    let sideArea = 2 * pi * r * h
-        topArea = pi * r ^2
-    in  sideArea + 2 * topArea
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+   in sideArea + 2 * topArea
 
 -- let <bindings> in <expression>
 -- unlike "where" bindings, which are just syntactic sugar, "let" bindings are
 -- expressions themselves
-
 -- for comparison, see an if statement, which can be stuck in anywhere:
 -- 4 * (if 10 > 5 then 10 else 0) + 2
-
 -- you can do the same with a let binding:
 -- 4 * (let a = 9 in a + 1) + 2
-
 -- can use for fn in local scope:
 -- [let square x = x * x in (square 5, square 3, square 2)]
 -- use ; to separate several vars inline:
 -- (let a = 100; b = 200; c = 300 in a*b*c, let foo="Hey "; bar = "there!" in foo ++ bar)
 -- can also use with pattern matching, as you can with "where" bindings:
 -- (let (a,b,c) = (1,2,3) in a+b+c) * 100
-
 -- can also use in list comprehensions, this is pretty neat
 calcBmis :: (RealFloat a) => [(a, a)] -> [a]
 calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
 
 -- why not always use a let binding? too local in scope; can't be used across
 -- guards
-
 -- case expressions: similar to cases in c/python, but also with pattern
 -- matching,  and they're expressions so you can use them anywhere
-
 -- syntax:
 {-
 case expression of pattern -> result
@@ -174,18 +181,22 @@ case expression of pattern -> result
 --
 --
 describeList :: [a] -> String
-describeList xs = "The list is " ++ case xs of [] -> "listless"
-                                               [x] -> "lonely :("
-                                               xs -> "crowded >:("
+describeList xs =
+  "The list is " ++
+  case xs of
+    [] -> "listless"
+    [x] -> "lonely :("
+    xs -> "crowded >:("
+
 -- recursion
 -- goes great w/ pattern matching
 -- hey, it's quicksort
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort (x:xs) =
-    let smallerSorted = quicksort [a | a <- xs, a <= x]
-        biggerSorted = quicksort [a | a <- xs, a > x]
-    in  smallerSorted ++ [x] ++ biggerSorted
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+   in smallerSorted ++ [x] ++ biggerSorted
 
 -- higher order fns take fn as parameter, return fn as values
 --
@@ -206,21 +217,20 @@ quicksort (x:xs) =
 -- a function with type sig a -> ordering:
 compareWithHundred :: (Num a, Ord a) => a -> Ordering
 compareWithHundred = compare 100
+
 -- this works b/c compare function has a type signature of:
 -- (Ord a) => a -> (a -> Ordering)
 --
 -- can also do with infix functions, this is called "sectioning":
 --
 divideByTen :: (Floating a) => a -> a
-divideByTen = (/10)
+divideByTen = (/ 10)
 
 -- functions can take functions as params too!
-
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
 
 -- () necessary because -> is right associative
-
 -- using higher order programming for useful things! here's an implementation
 -- of zipWith function. takes a function and two lists, joins the lists by
 -- applying function between corresponding elements
@@ -232,31 +242,29 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 -- another stdlib fn - flip
 flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f = g
-    where g x y = f y x
+  where
+    g x y = f y x
 
 -- maps and filters
-
 -- map applies a function to a list and gives you back a new list
 -- map :: (a -> b) -> [a] -> [b]
 --
 -- check this out:
 --
 -- map (map (^2)) [[1,2],[3,4,5,6],[7,8]]
-
 -- filter takes a predicate (fn returning boolean), a list, returns list
 -- satisfying the predicate
 -- filter :: (a -> Bool) -> [a] -> [a]
 --
 -- can use list comprehensions for all of these - which to use is decided on a
 -- per-case basis
-
 largestDivisible :: (Integral a) => a
-largestDivisible = head (filter p [100000,99999..])
-    where p x = x `mod` 3829 == 0
+largestDivisible = head (filter p [100000,99999 ..])
+  where
+    p x = x `mod` 3829 == 0
 
 -- takeWhile takes a predicate, list, keeps returning elem while predicate
 -- holds true, doesn't return anymore after that
-
 -- lambdas
 -- anonymous function, if you need it only once. usually made just to pass to a
 -- higher order function
@@ -268,7 +276,6 @@ largestDivisible = head (filter p [100000,99999..])
 --
 -- you may be better off using a partially applied function instead a lot of
 -- the time; saves space
-
 -- only folds and horses
 --
 -- common pattern: match on (x:xs), perform some work on single element, and
@@ -287,7 +294,6 @@ largestDivisible = head (filter p [100000,99999..])
 --
 --sum' :: (Num a) => [a] -> a
 --sum' xs = foldl (\acc x -> acc + x) 0 xs
-
 -- example with currying (similar to partially applied functions; viewing
 -- functions as list of functions applied 1 at a time)
 sum' :: (Num a) => [a] -> a
@@ -295,13 +301,19 @@ sum' = foldl (+) 0
 
 -- example with elem; check if something's an element of a list
 elem' :: (Eq a) => a -> [a] -> Bool
-elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+elem' y ys =
+  foldl
+    (\acc x ->
+       if x == y
+         then True
+         else acc)
+    False
+    ys
 
 -- generally, you can write, instead of "foo a = bar b a", write "foo = bar b"
 --
 -- foldr is similar, but binary function takes current value as first param and
 -- accumulator as second param instead
-
 -- map example with foldr
 map' :: (a -> b) -> [a] -> [b]
 map' f xs = foldr (\x acc -> f x : acc) [] xs
@@ -313,7 +325,6 @@ map' f xs = foldr (\x acc -> f x : acc) [] xs
 -- foldl1 and foldr1 are the same as foldr and foldr but assume first and last
 -- elements of list are starting values, so no need for explicit start.
 -- however, won't work on empty list
-
 -- fun reverse example:
 reverse' :: [a] -> [a]
 reverse' = foldl (flip (:)) []
@@ -326,7 +337,7 @@ reverse' = foldl (flip (:)) []
 -- fold. example/exercise: how many elements does it take for the sum of
 -- square roots of natural numbers to exceed 1000?
 sqrtSums :: Int
-sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
+sqrtSums = length (takeWhile (< 1000) (scanl1 (+) (map sqrt [1 ..]))) + 1
 
 -- function application with $
 -- normal fn application has a very high precedence, but $ has lowest
@@ -340,12 +351,10 @@ sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
 --ghci> map ($ 3) [(4+), (10*), (^2), sqrt]
         [7.0,30.0,9.0,1.7320508075688772]
 -}
-
 -- function composition f(g(x)) in math world is f . g in haskell world
 -- right associative
 -- useful example:
 -- ghci> map (negate . sum . tail) [[1..5],[3..6],[1..7]]
-
 -- when you write a function without rightmost argument, you write it in a
 -- "point free style"
 --
@@ -356,7 +365,6 @@ sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
 -- fn = ceiling . negate . tan . cos . max 50
 --
 -- can't just remove x on the right because of all the parentheses
-
 -- modules
 -- collections of related functions, types, and typeclasses. program is
 -- collection of modules. main module loads up others and uses functions to do
@@ -470,7 +478,6 @@ sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
 -- so compare `on` length gives back something like this:
 -- compare (length x) (length y)
 -- which returns an Ordering, so you can give that to the sortBy function
-
 -- next up, Data.Char module
 --
 -- tons of predicate Char -> Bool functions
@@ -490,6 +497,7 @@ sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
 -- ord/chr map characters to numbers
 -- very elegant caesar cipher:
 encode shift msg = map (chr . (+ shift) . ord) msg
+
 decode shift msg = encode (negate shift) msg
 
 -- Data.Map
@@ -521,7 +529,6 @@ decode shift msg = encode (negate shift) msg
 -- can do some nice things like passing max function here
 --
 -- similarly, there's Map.insertWith
-
 -- Data.Set
 -- internally implemented with trees, so they're ordered. ordering may not be
 -- the same as the list when constructing a Set from fromList, though - it'll
@@ -533,13 +540,11 @@ decode shift msg = encode (negate shift) msg
 --
 -- de-duplicating with sets much faster than nub, but xs have to be Ord. you'll
 -- also lose list's original ordering with Set.
-
 -- see Geometry.hs for example of creating your own module, pretty
 -- straightforward
 -- also striaghtforward for more complicated hierarhical namespacing - create
 -- Geometry folder with files Sphere, Cube, Cuboid, call them Geometry.Sphere,
 -- Geometry.Cube ... in headers
-
 -- Making our own types and typeclasses
 -- algebraic data types intro - how to make your own types?
 -- like this!
@@ -565,11 +570,23 @@ surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
 -- typeclass, do:
 -- data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
 -- can make it a little more abstract, too:
-data Point = Point Float Float deriving (Show)
-data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+data Point =
+  Point Float
+        Float
+  deriving (Show)
+
+data Shape
+  = Circle Point
+           Float
+  | Rectangle Point
+              Point
+  deriving (Show)
+
 -- then you have to modify surface:
 surface (Circle _ r) = pi * r ^ 2
-surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+surface (Rectangle (Point x1 y1) (Point x2 y2)) =
+  (abs $ x2 - x1) * (abs $ y2 - y1)
+
 -- to export your data types, write type with value constructors in parens:
 -- module Shapes
 -- ( Point(..)
@@ -580,13 +597,13 @@ surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - 
 -- exporting any value constructors. Map.Map does this.
 --
 -- Record syntax: alternative way to write data types with named components
-data Person = Person { firstName :: String
+{- data Person = Person { firstName :: String
                      , lastName :: String
                      , age :: Int
                      , height :: Float
                      , phoneNumber :: String
                      , flavor :: String
-                     } deriving (Show)
+                     } deriving (Show) -}
 -- haskell automatically creates all those functions for you
 -- now you can do this, and even put fields out of order:
 -- Person {lastName="V", firstName="Mario", age=100, height=42.0, phoneNumber="123-4567", flavor="Arugula"}
@@ -613,3 +630,69 @@ data Person = Person { firstName :: String
 --
 -- Derived instances
 -- Typeclasses are more like interfaces in other languages than classes.
+-- Below will automatically give you == and /= on a  Person type, assuming all
+-- values in type can be equaled.
+data Person = Person
+  { firstName :: String
+  , lastName :: String
+  , age :: Int
+  } deriving (Eq, Show, Read)
+
+-- we can now use a Person in all functions that have the Eq class constraint
+-- for the parameter
+-- Read typeclass is basically inverse of Show. We can do:
+mikeD =
+  read "Person {firstName =\"Michael\", lastName =\"Diamond\", age = 43}" :: Person
+
+-- must use explicit type annotation since Haskell doesn't know what the result
+-- of read will be before it executes
+-- if you use the result of read later in a way that it has to be a Person,
+-- Haskell will know that it should expect a Person
+-- for deriving from Ord, if you have two values done with different
+-- constructors, the first one in the type definition is considered smaller
+-- data Bool = False | True deriving (Ord)
+-- ^ in there, False < True
+-- with Maybe a data type, Nothing is always less than Just, but if you have two
+-- Justs, it goes inside the Just and compares the values directly according to
+-- the type paramter.
+-- bigger example:
+data Day
+  = Monday
+  | Tuesday
+  | Wednesday
+  | Thursday
+  | Friday
+  | Saturday
+  | Sunday
+  deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+-- Bounded types have lowest and highest possible value
+-- Enum have predecessors and successors
+-- Type synonyms
+-- just different names
+-- type String = [Char]
+-- here's a useful example of using this to make things more readable
+type PhoneNumber = String
+
+type Name = String
+
+type PhoneBook = [(Name, PhoneNumber)]
+
+-- now you can use those in function definitions to give it a very pretty type
+-- declaration
+-- you can also parameterize type synonyms
+type AssocList k v = [(k, v)]
+
+-- you can also partially apply type parameters to get new type constructors
+-- from them, just like you can partially apply functions
+type IntMap v = Map.Map Int v-- now you can do this:
+-- Map.fromList [(1, "hi"), (2, "bye")] :: IntMap String
+-- another cool data type that takes two types as parameters: Either a b type
+-- roughly:
+-- data Either a b = Left a | Right b deriving (Eq, Ord, Read, Show)
+-- good to use instead of Maybe in some cases, because Maybe doesn't indicate
+-- *how* something failed. a is a type that can tell us about possible failure
+-- and b is type of succesful computation. so, errors use Left value constructor
+-- and results use Right.
+-- book has example with "locker lookup" functions
+-- Recursive data structures
